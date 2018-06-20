68939ba3e45f6e7a726920cf3489df879c404747
Lasse Silkoset <lassesilkoset@Lasses-MacBook-Pro.local>
2018-06-20 02:57:59 +0200

    caching on all three funcs

--- GiantLeap/Controller/SearchViewController.swift
+++ GiantLeap/Controller/SearchViewController.swift
@@ -34,28 +34,28 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     private var size = 10
     private var cellForIndex: IndexPath?
     
-
+    
     @IBOutlet weak var segmentedControlOutlet: UISegmentedControl!
     @IBOutlet weak var searchBarOutlet: UISearchBar!
     @IBOutlet weak var searchTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         self.navigationItem.title = "Search"
         searchTableView.delegate = self
         searchTableView.dataSource = self
         searchTableView.isUserInteractionEnabled = true
         searchTableView.separatorStyle = .none
         searchBarOutlet.delegate = self
-
-       
+        
+        
     }
     
-
+    
     
     //PRAGMA MARK: - Tableview Funcs ********************************************************
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -65,14 +65,14 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         cell.nameLabel.text = itemArray[indexPath.row].name
         if let orgnr = itemArray[indexPath.row].org {
-        cell.orgLabel.text = String(orgnr)
+            cell.orgLabel.text = String(orgnr)
         }
         if let companyAddress = itemArray[indexPath.row].adresse {
             cell.adressLabel.text = companyAddress
         } else {
             cell.adressLabel.isHidden = true
         }
-      
+        
         return cell
     }
     
@@ -89,7 +89,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-
+        
         if scrollView == searchTableView {
             if ((scrollView.contentOffset.y + scrollView.frame.size.height) >= scrollView.contentSize.height) {
                 size += 10
@@ -105,7 +105,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         page = 0
         size = 10
         
-
+        
         return true
     }
     
@@ -120,7 +120,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         switch segmentedControlOutlet.selectedSegmentIndex {
         case 0:
-           
+            
             //API-et støtter kun søk på minimum ni sifre på orgnr
             if searchText.count > 8 {
                 SearchTextFromBar = searchText
@@ -137,7 +137,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
     
-   
+    
     
     //PRAGMA MARK: - Networking Funcs ********************************************************
     
@@ -150,66 +150,30 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         
         var tempItemArray = [ItemObject]()
         
-        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+        
             if let orgnr = searchName {
                 let finalURL = "http://data.brreg.no/enhetsregisteret/enhet/\(orgnr).json"
-                
-                guard let itemUrl = URL(string: finalURL) else { return }
-                print(itemUrl)
-                if let urlContents = try? Data(contentsOf: itemUrl) {
-                    
-                    do {
-                        let item = try JSONDecoder().decode(OrgJson.self, from: urlContents)
-                        
-                        tempItemArray.append(ItemObject(name: item.navn, org: item.organisasjonsnummer, adresse: item.forretningsadresse?.adresse, naering: item.naeringskode1?.beskrivelse, postnr: item.forretningsadresse?.postnummer, poststed: item.forretningsadresse?.poststed, hjemmeside: item.hjemmeside))
-                        
-                        if orgnr == self?.SearchTextFromBar {
-                            DispatchQueue.main.async {
-                                self?.itemArray.removeAll()
-                                completion(.Success(tempItemArray))
-                            }
-                        }
-                    } catch {
-                        print(error)
-                        
-                    }
-                } else {
-                    completion(.Error("Unable to retrieve data from URL"))
-                }
-            }
-        }
-    }
-    
-    private func fetchItemsByName(searchName: String?, completion: @escaping (Result<[ItemObject]>) -> Void) {
-        
-       var tempItemArray = [ItemObject]()
-        let tempStr = size
-        let tempPage = page
-   
-            if let filter = searchName {
-                let finalURL = "http://data.brreg.no/enhetsregisteret/enhet.json?page=\(tempPage)&size=\(tempStr)&$filter=startswith(navn,'\(filter)')"
                 
                 guard let itemUrl = URL(string: finalURL) else { return }
                 
                 let request = URLRequest(url: itemUrl, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 60)
                 
                 URLSession.shared.dataTask(with: request) { (data, response, error) in
-                   
+                    
                     
                     if error != nil {
                         print(error!.localizedDescription)
+                        completion(.Error("Unable to retrieve data from URL"))
                     }
                     
                     guard let data = data else { return }
                     
                     do {
-                        let items = try JSONDecoder().decode(NameJson.self, from: data)
-                        for item in items.data{
-                            tempItemArray.append(ItemObject(name: item.navn, org: item.organisasjonsnummer, adresse: item.forretningsadresse?.adresse, naering: item.naeringskode1?.beskrivelse, postnr: item.forretningsadresse?.postnummer, poststed: item.forretningsadresse?.poststed, hjemmeside: item.hjemmeside))
-                                                   }
-                        //Get back to the main queue
+                        let item = try JSONDecoder().decode(OrgJson.self, from: data)
+                       
+                        tempItemArray.append(ItemObject(name: item.navn, org: item.organisasjonsnummer, adresse: item.forretningsadresse?.adresse, naering: item.naeringskode1?.beskrivelse, postnr: item.forretningsadresse?.postnummer, poststed: item.forretningsadresse?.poststed, hjemmeside: item.hjemmeside))
+                        
                         DispatchQueue.main.async {
-                            
                             self.itemArray.removeAll()
                             completion(.Success(tempItemArray))
                         }
@@ -218,6 +182,47 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
                     }
                     }.resume()
             }
+        
+    }
+    
+    private func fetchItemsByName(searchName: String?, completion: @escaping (Result<[ItemObject]>) -> Void) {
+        
+        var tempItemArray = [ItemObject]()
+        let tempStr = size
+        let tempPage = page
+        
+        if let filter = searchName {
+            let finalURL = "http://data.brreg.no/enhetsregisteret/enhet.json?page=\(tempPage)&size=\(tempStr)&$filter=startswith(navn,'\(filter)')"
+            
+            guard let itemUrl = URL(string: finalURL) else { return }
+            
+            let request = URLRequest(url: itemUrl, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 60)
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                
+                if error != nil {
+                    print(error!.localizedDescription)
+                    completion(.Error("Unable to retrieve data from URL"))
+                }
+                
+                guard let data = data else { return }
+                
+                do {
+                    let items = try JSONDecoder().decode(NameJson.self, from: data)
+                    for item in items.data{
+                        tempItemArray.append(ItemObject(name: item.navn, org: item.organisasjonsnummer, adresse: item.forretningsadresse?.adresse, naering: item.naeringskode1?.beskrivelse, postnr: item.forretningsadresse?.postnummer, poststed: item.forretningsadresse?.poststed, hjemmeside: item.hjemmeside))
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self.itemArray.removeAll()
+                        completion(.Success(tempItemArray))
+                    }
+                } catch let jsonError {
+                    print(jsonError)
+                }
+            }.resume()
+        }
     }
     
     private func fetchItemsByAdress(searchName: String?, completion: @escaping (Result<[ItemObject]>) -> Void) {
@@ -226,40 +231,42 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         let tempStr = size
         let tempPage = page
         
-        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            if let adress = searchName {
-                let finalURL = "http://data.brreg.no/enhetsregisteret/enhet.json?page=\(tempPage)&size=\(tempStr)&$filter=startswith(forretningsadresse/adresse,'\(adress)')"
-                
-                guard let itemUrl = URL(string: finalURL) else { return }
-                print(itemUrl)
-                if let urlContents = try? Data(contentsOf: itemUrl) {
-                    
-                    do {
-                        let items = try JSONDecoder().decode(NameJson.self, from: urlContents)
-                        for item in items.data{
-                            tempItemArray.append(ItemObject(name: item.navn, org: item.organisasjonsnummer, adresse: item.forretningsadresse?.adresse, naering: item.naeringskode1?.beskrivelse, postnr: item.forretningsadresse?.postnummer, poststed: item.forretningsadresse?.poststed, hjemmeside: item.hjemmeside))
-                        }
-                        if adress == self?.SearchTextFromBar {
-                            DispatchQueue.main.async {
-                                self?.itemArray.removeAll()
-                                completion(.Success(tempItemArray))
-                            }
-                        }
-                    } catch {
-                        print(error)
-                        
-                    }
-                } else {
+        
+        if let address = searchName {
+            let finalURL = "http://data.brreg.no/enhetsregisteret/enhet.json?page=\(tempPage)&size=\(tempStr)&$filter=startswith(forretningsadresse/adresse,'\(address)')"
+            
+            guard let itemUrl = URL(string: finalURL) else { return }
+            
+            let request = URLRequest(url: itemUrl, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 60)
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if error != nil {
+                    print(error!.localizedDescription)
                     completion(.Error("Unable to retrieve data from URL"))
                 }
-            }
+                
+                guard let data = data else { return }
+                
+                do {
+                    let items = try JSONDecoder().decode(NameJson.self, from: data)
+                    for item in items.data{
+                        tempItemArray.append(ItemObject(name: item.navn, org: item.organisasjonsnummer, adresse: item.forretningsadresse?.adresse, naering: item.naeringskode1?.beskrivelse, postnr: item.forretningsadresse?.postnummer, poststed: item.forretningsadresse?.poststed, hjemmeside: item.hjemmeside))
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self.itemArray.removeAll()
+                        completion(.Success(tempItemArray))
+                    }
+                } catch let jsonError {
+                    print(jsonError)
+                }
+            }.resume()
         }
-        
     }
     
-
+    
     //PRAGMA MARK: - Helper Funcs ********************************************************
-
+    
     private func fetchItems() {
         switch segmentedControlOutlet.selectedSegmentIndex {
         case 0:
@@ -268,7 +275,7 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
                 self.handlingResult(result: result)
             })
         case 1:
-    
+            
             fetchItemsByName(searchName: SearchTextFromBar, completion: { (result) in
                 self.handlingResult(result: result)
             })
@@ -284,12 +291,12 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     private func handlingResult(result: Result<[ItemObject]>) {
         switch result {
         case .Success(let item):
-                self.itemArray = item
+            self.itemArray = item
         case .Error(let error):
-           print(error)
-           DispatchQueue.main.async {
-            self.presentAlert(error: error)
-           }
+            print(error)
+            DispatchQueue.main.async {
+                self.presentAlert(error: error)
+            }
             
         }
     }
@@ -300,17 +307,17 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         
         self.present(alert, animated: true, completion: nil)
-
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "infoSegue" {
             if let index = cellForIndex {
-            let _ = searchTableView.cellForRow(at: index)
+                let _ = searchTableView.cellForRow(at: index)
                 let destinationVC = segue.destination as? InfoViewController
                 print("kom hit i prepare")
                 destinationVC?.infoArray.append(itemArray[index.row])
-               
+                
             }
         }
     }
